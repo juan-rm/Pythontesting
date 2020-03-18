ba4e3886b782399d87e4a430ae38a63fbdb3c9a1
juan-rm <jrmtel@gmail.com>
2020-03-18 09:41:09 +0100

    esto es un programa para pasar listas a una funcion

---
+++ pasar_lista.py
@@ -0,0 +1,7 @@
+def names (name) :
+# imprimimos los nombres pasados por la lista
+    for nombre in name :
+        print("Hola me encanta conocerte, " + nombre.title())
+
+lista_nombres = ["maria" , "sara" , "pepi"]
+names(lista_nombres)
